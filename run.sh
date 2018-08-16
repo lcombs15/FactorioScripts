f95d50d9265b55ab67afe443ae31a86e45eab90b
Lucas Combs <l-combs@hotmail.com>
2018-08-16 13:25:54 -0400

    Cleanup for run script
    	- Better comments
    	- shorter echo command

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-echo >> server_log.txt
-echo >> server_log.txt
-echo $(date) >> server_log.txt
+#!/bin/bash
+# run.sh
+# Lucas Combs
+# 16 August 2018
+# github.com/lcombs15
 
+# Start factorio server
+
+# Append two empty lines and current datetime to server log
+echo -e "\n" $(date) >> server_log.txt
+
+# Start server in background
 nohup ./server_exe/bin/x64/factorio --start-server saves/squad.zip >> server_log.txt &
-
-exit 0
